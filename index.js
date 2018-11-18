68ccc94290221f2a1cf952b0e39efc5e4ed85915
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-18 00:19:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object,key) {
   return object;
 }
 
-function destructivelyDeleteFromObjectByKey(objec,key) {
+function destructivelyDeleteFromObjectByKey(object,key) {
   delete object[key];
   return object;
 }
